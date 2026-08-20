76ab735741885c8a20ad4ba031f8e54f2342875f
2004rlarkawk-bit <2004rlarkawk-bit@users.noreply.github.com>
2026-08-20 05:34:49 +0000

    deploy: 8e7e25fd4068e2ba8aee92b30426a0f8b359f786

---
+++ assets/ImportForwarderFlow-y_cBnf_9.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BVMaVagc.js";import{I as t}from"./ImportTradeFlow-CCk630Vu.js";import"./icons-2oWDHVY9.js";import"./react-lOp5rQZT.js";import"./importDocumentAnalysisService-MSOadR33.js";import"./index-BFBroWeK.js";function f(r){return o.jsx(t,{role:"forwarder",...r})}export{f as default};
