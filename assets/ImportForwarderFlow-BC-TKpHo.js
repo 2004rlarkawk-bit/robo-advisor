3e56e3d6c3ca8d5e53089542114b3458261d282b
2004rlarkawk-bit <2004rlarkawk-bit@users.noreply.github.com>
2026-08-20 04:12:09 +0000

    deploy: 6eb2da84fb574286f59a89ec9c2a9e05511db9fb

--- assets/ImportForwarderFlow-BC-TKpHo.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DX0d_RHw.js";import{I as t}from"./ImportTradeFlow-BstgtJp0.js";import"./icons-2oWDHVY9.js";import"./react-lOp5rQZT.js";import"./importDocumentAnalysisService-BDosGj10.js";import"./index-BFBroWeK.js";function f(r){return o.jsx(t,{role:"forwarder",...r})}export{f as default};
