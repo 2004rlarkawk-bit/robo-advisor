6a7814676af87a1bcbeb6caf89f110e1414dcfa8
2004rlarkawk-bit <2004rlarkawk-bit@users.noreply.github.com>
2026-09-09 09:13:28 +0000

    deploy: 446c4821f20f6084bb035bc0ca9b8ada3586e605

---
+++ assets/ImportShipperFlow-tTQJm699.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BuArtJUK.js";import{I as p}from"./ImportTradeFlow-BHnOaov1.js";import"./icons-MR4sN3Xe.js";import"./react-1fSbKeuo.js";import"./importDocumentAnalysisService-BPfLBByF.js";import"./index-D9WW1lr0.js";function f(r){return o.jsx(p,{role:"shipper",...r})}export{f as default};
