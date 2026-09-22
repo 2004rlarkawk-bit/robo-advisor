9c5d1cc5f6204be9edba99b4316aa206d41de5dc
2004rlarkawk-bit <2004rlarkawk-bit@users.noreply.github.com>
2026-09-22 10:25:43 +0000

    deploy: 943da1b9ce4e0ceb0388b828426b86f3ee7dc990

---
+++ assets/ImportForwarderFlow-BUyzEO8F.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-JFYiLJP9.js";import{I as t}from"./ImportTradeFlow-DkwH8AkE.js";import"./icons-BOIr0Iwu.js";import"./react-CWP7Dy88.js";import"./ImportStepIndicator-E96zwPHv.js";import"./cargoProgressService-DCuiSXFS.js";import"./docxtemplater-DdPgm2Vn.js";import"./docx-preview-CrDSuFE7.js";function l(r){return o.jsx(t,{role:"forwarder",...r})}export{l as default};
