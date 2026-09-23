6db777b29fa9ed55e123ee08c30ad9be78fb6ecc
bohyeonkang37 <bohyeonkang37@users.noreply.github.com>
2026-09-23 14:05:43 +0000

    deploy: f5d70f9e0d5fa7c8c8feda3f5f379812540c3e53

---
+++ assets/ImportShipperFlow-CtJF7hxB.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-C9MS-RUW.js";import{I as p}from"./ImportTradeFlow-e-0fM-_v.js";import"./icons-CDCi_Q8P.js";import"./react-f9MksVKi.js";import"./ImportStepIndicator-uMabbCqv.js";import"./cargoProgressService-CKM60kLH.js";import"./docx-preview-ceJKexXa.js";function l(r){return o.jsx(p,{role:"shipper",...r})}export{l as default};
